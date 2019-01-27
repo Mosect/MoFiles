e1ae95c3550f06236e13fb78db4527587eccf5da
Mosect <zhouliuyang1995@163.com>
2019-01-27 17:33:17 +0800

    优化目录

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 // 文件配置（记录信息）目录
-const CONFIG_DIR = __DIR__ . '/../config/';
+const CONFIG_DIR = __DIR__ . '/info/';
 // 文件保存目录
-const FILES_DIR = __DIR__ . '/../files/';
+const FILES_DIR = __DIR__ . '/files/';
 ?>
